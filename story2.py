a9f0f2ad84450e006d4d021725a46334f10818fb
peter <peter.russell@kearsarge.org>
2018-09-27 08:25:57 -0400

    story 2

---
+++ story2.py
@@ -0,0 +1,12 @@
+from getInput import *
+
+
+def playMadlibs():
+    friend1 = getWord("Enter a Name: ")
+    numAnimals = getNumber("Enter a number: ", 2, 10)
+    animals1 = getWord("Enter a pluaral animal name: ")
+    pronoun1 = getPro("Enter a singular pronoun")
+    
+    output = ""
+    output+= "
+    return output
