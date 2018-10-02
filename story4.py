3b48862614808f57a96cbfc5f874453f2a04bac3
pdrussell <peter.russell@kearsarge.org>
2018-10-02 08:05:20 -0400

    story 4 edits

--- story4.py
+++ story4.py
@@ -2,8 +2,15 @@ from getInput import *
 
 
 def playMadlibs():
-    
+    day1 = 
     
     output = " "
-    
+    output += "One " + day1
+    output += " morning my friend, " + friend1
+    output += ", and I were walking down the street of " + kearsargeTown
+    output += ". When we saw " + friend2
+    output += ' and said "' + interjection
+    output += ' you look awful!" '
+    output += '"Im sick" said ' + friend2
+    output += " "
     return output
